0416ecb8f54784a7b6cb832c0a1913f2f13d079e
NicholasWessinger <nicholaswessingerwork@gmail.com>
2021-07-29 10:19:24 -0400

    15 sec 2x boost

--- Assets/Nicholas/Scripts/ScoreScript.cs
+++ Assets/Nicholas/Scripts/ScoreScript.cs
@@ -3,29 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class ScoreScript : MonoBehaviour
 {
     public Text MyscoreText;
     public Text WorkerBeePriceText;
     public Text QualityPollenPriceText;
+    public Text BoostPriceText;
     int ScoreNum = 0;
     int workerBeePrice = 10;
     int qualityPollenPrice = 5;
-    int addOn = 0;
+    int boostPrice = 50;
+    int addOn = 1;
     int holder = 0;
     int holdertwo = 0;
-    int adding = 0;
+    int holderthree = 0;
+    int adding = 1;
+    private float startTime;
+    private float boostStartTime;
+    private float timer;
 
     private void Start()
     {
         MyscoreText.text = "" + ScoreNum;
         WorkerBeePriceText.text = "Price: $" + workerBeePrice;
         QualityPollenPriceText.text = "Price: $" + qualityPollenPrice;
+        BoostPriceText.text = "Price: $" + boostPrice;
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        float t = Time.time - startTime;
+
+        timer = t / 60;
     }
 
     public void clickScoreAdd()
     {
-        ScoreNum += 1 + adding;
+        ScoreNum += adding;
         MyscoreText.text = "" + ScoreNum;
     } 
 
@@ -39,7 +55,7 @@ public class ScoreScript : MonoBehaviour
             workerBeePrice += 90;
             WorkerBeePriceText.text = "Price: $" + workerBeePrice;
             InvokeRepeating("workers", 1f, 1f);
-            addOn += 1;
+            addOn += 0;
             holder += 1;
         }
 
@@ -125,5 +141,73 @@ public class ScoreScript : MonoBehaviour
 
         }
 
+    }
+
+    
+
+    public void Boost()
+    {
+        if (ScoreNum >= boostPrice && holderthree == 0)
+        {
+            ScoreNum -= 50;
+            MyscoreText.text = "" + ScoreNum;
+            boostPrice += 100;
+            BoostPriceText.text = "Price: $" + boostPrice;
+            holderthree += 1;
+
+            addOn *= 2;
+            adding *= 2;
+            InvokeRepeating("nonBoost", 15f, 1f);
+
+        }
+
+        if (ScoreNum >= boostPrice && holderthree == 1)
+        {
+            ScoreNum -= 150;
+            MyscoreText.text = "" + ScoreNum;
+            boostPrice += 150;
+            BoostPriceText.text = "Price: $" + boostPrice;
+            holderthree += 1;
+
+            addOn *= 2;
+            adding *= 2;
+            InvokeRepeating("nonBoost", 15f, 1f);
+        }
+
+        if (ScoreNum >= boostPrice && holderthree == 2)
+        {
+            ScoreNum -= 300;
+            MyscoreText.text = "" + ScoreNum;
+            boostPrice += 200;
+            BoostPriceText.text = "Price: $" + boostPrice;
+            holderthree += 1;
+
+            addOn *= 2;
+            adding *= 2;
+            InvokeRepeating("nonBoost", 15f, 1f);
+        }
+
+        if (ScoreNum >= boostPrice && holderthree == 3)
+        {
+            ScoreNum -= 500;
+            MyscoreText.text = "" + ScoreNum;
+            boostPrice += 500;
+            BoostPriceText.text = "Price: $" + boostPrice;
+            holderthree += 1;
+
+            addOn *= 2;
+            adding *= 2;
+            InvokeRepeating("nonBoost", 15f, 1f);
+        }
+
+
+    }
+
+    public void nonBoost()
+    {
+        addOn /= 2;
+        adding /= 2;
+        MyscoreText.text = "" + ScoreNum;
+        CancelInvoke("nonBoost");
     }
 }
